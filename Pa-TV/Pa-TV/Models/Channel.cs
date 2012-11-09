050d396111d7fb61c363906270d1edf677114469
Tore Lervik <tore@mindre.net>
2012-11-09 19:50:18 +0100

    added models

---
+++ Pa-TV/Pa-TV/Models/Channel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pa_TV.Models
+{
+    public class Channel
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public string LogoUrl { get; set; }
+    }
+}
---
+++ Pa-TV/Pa-TV/Models/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pa_TV.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public int GenreId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
